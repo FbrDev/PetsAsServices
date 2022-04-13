ce90008cf61376ade1a93bd3d500ad30a2d2d6ac
Fabrício Pereira da Conceição <fabriciopereiradev@gmail.com>
2022-04-13 14:44:25 -0300

    ADD: Capturando dados da API e criando lógica na tela Buscar Raças

--- frmBuscarRaca.Designer.cs
+++ frmBuscarRaca.Designer.cs
@@ -36,14 +36,14 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBoxGatinho = new System.Windows.Forms.PictureBox();
+            this.cbRaca = new System.Windows.Forms.ComboBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnFavoritar = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.lblResult1 = new System.Windows.Forms.Label();
+            this.lblResult2 = new System.Windows.Forms.Label();
+            this.lblResult3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGatinho)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -92,7 +92,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(131, 182);
+            this.label5.Location = new System.Drawing.Point(41, 182);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(139, 20);
             this.label5.TabIndex = 4;
@@ -102,7 +102,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(194, 219);
+            this.label6.Location = new System.Drawing.Point(403, 182);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(76, 20);
             this.label6.TabIndex = 5;
@@ -112,32 +112,32 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(169, 258);
+            this.label7.Location = new System.Drawing.Point(41, 239);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(101, 20);
             this.label7.TabIndex = 6;
             this.label7.Text = "Descrição:";
             // 
-            // pictureBox1
+            // pictureBoxGatinho
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(18, 309);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(619, 242);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 7;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxGatinho.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGatinho.Image")));
+            this.pictureBoxGatinho.Location = new System.Drawing.Point(12, 410);
+            this.pictureBoxGatinho.Name = "pictureBoxGatinho";
+            this.pictureBoxGatinho.Size = new System.Drawing.Size(258, 184);
+            this.pictureBoxGatinho.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGatinho.TabIndex = 7;
+            this.pictureBoxGatinho.TabStop = false;
             // 
-            // comboBox1
+            // cbRaca
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(299, 130);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(180, 24);
-            this.comboBox1.TabIndex = 8;
-            this.comboBox1.Text = "Selecione uma Raça";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.cbRaca.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbRaca.FormattingEnabled = true;
+            this.cbRaca.Location = new System.Drawing.Point(299, 130);
+            this.cbRaca.Name = "cbRaca";
+            this.cbRaca.Size = new System.Drawing.Size(180, 24);
+            this.cbRaca.TabIndex = 8;
+            this.cbRaca.Text = "Selecione uma Raça";
+            this.cbRaca.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // btnBuscar
             // 
@@ -147,6 +147,7 @@
             this.btnBuscar.TabIndex = 9;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // btnFavoritar
             // 
@@ -157,35 +158,34 @@
             this.btnFavoritar.Text = "Favoritar";
             this.btnFavoritar.UseVisualStyleBackColor = true;
             // 
-            // label8
+            // lblResult1
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(295, 182);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(84, 20);
-            this.label8.TabIndex = 11;
-            this.label8.Text = "Resultado";
+            this.lblResult1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult1.Location = new System.Drawing.Point(186, 182);
+            this.lblResult1.Name = "lblResult1";
+            this.lblResult1.Size = new System.Drawing.Size(211, 43);
+            this.lblResult1.TabIndex = 11;
+            this.lblResult1.Text = "Resultado";
+            this.lblResult1.Click += new System.EventHandler(this.label8_Click);
             // 
-            // label9
+            // lblResult2
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(295, 219);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(84, 20);
-            this.label9.TabIndex = 12;
-            this.label9.Text = "Resultado";
+            this.lblResult2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult2.Location = new System.Drawing.Point(485, 182);
+            this.lblResult2.Name = "lblResult2";
+            this.lblResult2.Size = new System.Drawing.Size(152, 43);
+            this.lblResult2.TabIndex = 12;
+            this.lblResult2.Text = "Resultado";
+            this.lblResult2.Click += new System.EventHandler(this.label9_Click);
             // 
-            // label10
+            // lblResult3
             // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(295, 258);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(84, 20);
-            this.label10.TabIndex = 13;
-            this.label10.Text = "Resultado";
+            this.lblResult3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult3.Location = new System.Drawing.Point(148, 239);
+            this.lblResult3.Name = "lblResult3";
+            this.lblResult3.Size = new System.Drawing.Size(489, 156);
+            this.lblResult3.TabIndex = 13;
+            this.lblResult3.Text = "Resultado";
             // 
             // frmBuscarRaca
             // 
@@ -194,13 +194,13 @@
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(668, 605);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblResult3);
+            this.Controls.Add(this.lblResult2);
+            this.Controls.Add(this.lblResult1);
             this.Controls.Add(this.btnFavoritar);
             this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbRaca);
+            this.Controls.Add(this.pictureBoxGatinho);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -213,7 +213,8 @@
             this.Name = "frmBuscarRaca";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Buscar Raças";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.frmBuscarRaca_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGatinho)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,12 +229,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBoxGatinho;
+        private System.Windows.Forms.ComboBox cbRaca;
         private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button btnFavoritar;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblResult1;
+        private System.Windows.Forms.Label lblResult2;
+        private System.Windows.Forms.Label lblResult3;
     }
 }
--- frmBuscarRaca.cs
+++ frmBuscarRaca.cs
@@ -1,23 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace PetsAsServices
 {
     public partial class frmBuscarRaca : Form
     {
+        private string _name { get; set;}
+
         public frmBuscarRaca()
         {
             InitializeComponent();
         }
 
+        private void frmBuscarRaca_Load(object sender, EventArgs e)
+        {
+            IncluiItems();
+        }
+
+        private async void IncluiItems()
+        {
+            var client = new RestClient($"https://api.thecatapi.com/v1/breeds");
+            RestRequest request = new RestRequest("", Method.Get);
+            var response = await client.ExecuteGetAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var content = response.Content;
+                List<CatJsonToClass> myDeserializedObjList = (List<CatJsonToClass>)JsonConvert.DeserializeObject(content, typeof(List<CatJsonToClass>));
+
+                foreach (var obj in myDeserializedObjList)
+                {
+                    cbRaca.Items.Add(obj.name);
+                }
+            }
+            else
+            {
+                var error = response.ErrorMessage;
+            }
+        }
+
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _name = cbRaca.SelectedItem.ToString();
+        }
+
+        private async void btnBuscar_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient($"https://api.thecatapi.com/v1/breeds/search?q={_name}");
+            RestRequest request = new RestRequest("", Method.Get);
+            var response = await client.ExecuteGetAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var content = response.Content;
+                List<CatJsonToClass> myDeserializedObjList = (List<CatJsonToClass>)JsonConvert.DeserializeObject(content, typeof(List<CatJsonToClass>));
+
+                foreach (var myDeserializedObj in myDeserializedObjList)
+                {
+                    lblResult1.Text = myDeserializedObj.temperament;
+                    lblResult2.Text = myDeserializedObj.origin;
+                    lblResult3.Text = myDeserializedObj.description;
+                }
+            }
+            else
+            {
+                var error = response.ErrorMessage;
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
         {
 
         }
